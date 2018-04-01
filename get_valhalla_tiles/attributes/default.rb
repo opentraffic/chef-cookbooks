c65143bc52dfa2bdf071e33d9d75a9c7b7cd2fea
Steven Ottens <github@minst.net>
2018-04-01 19:24:10 +0200

    newer planet file

--- get_valhalla_tiles/attributes/default.rb
+++ get_valhalla_tiles/attributes/default.rb
@@ -3,4 +3,4 @@ default[:get_valhalla_tiles][:log_dir]  = "#{node[:get_valhalla_tiles][:base_dir
 default[:get_valhalla_tiles][:tile_dir] = "#{node[:get_valhalla_tiles][:base_dir]}/valhalla"
 
 default[:get_valhalla_tiles][:s3bucket]  = 'reporter-tiles'
-default[:get_valhalla_tiles][:s3file]    = 'planet_2018_03_19-11_17_33/planet_2018_03_19-11_17_33.tar'
+default[:get_valhalla_tiles][:s3file]    = 'planet_2018_03_31-00_33_54/planet_2018_03_31-00_33_54.tar'
